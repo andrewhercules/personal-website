74adfd7e23bc95efb64e673ebdef061f1d568524
Andrew Hercules <andrewhercules@gmail.com>
2014-11-20 12:43:54 +0000

    Implemented Sinatra partials for header and nav bar and created new portfolio.erb view

--- server.rb
+++ server.rb
@@ -7,3 +7,15 @@ set :public_folder, Proc.new { File.join(root, '.', 'public') }
 get '/' do
   erb :index
 end
+
+get '/portfolio' do
+  erb :portfolio
+end
+
+get '/CV' do
+end
+
+get '/contact/' do
+  erb :contact
+end
+
