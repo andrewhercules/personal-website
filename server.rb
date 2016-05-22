7da0510b4d2d02f4e4eb132d10425bb8e7f9b0ed
Andrew Hercules <andrewhercules@gmail.com>
2016-05-22 17:34:34 +0100

    Updated server.rb and removed routes no longer needed

--- server.rb
+++ server.rb
@@ -1,19 +1,9 @@
 require 'sinatra'
-require 'sinatra/partial'
 
-set :partial_template_engine, :erb
 set :public_folder, Proc.new { File.join(root, '.', 'public') }
 
 get '/' do
   erb :index
-end
-
-get '/about' do
-  erb :about
-end
-
-get '/portfolio' do
-  erb :portfolio
 end
 
 not_found do
