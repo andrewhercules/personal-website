e13af4b860366c7047c53a1bf254b4022853a784
Andrew Hercules <andrewhercules@gmail.com>
2016-05-22 17:05:49 +0100

    Removed files used by previous layout

--- public/js/interface.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-
-  // var images = ['homepage1.jpg', 'homepage2.jpg', 'homepage3.jpg'];
-  // var randomNumber = Math.floor(Math.random() * images.length)
-  // var randomImage = images[randomNumber]
-  // console.log(randomNumber)
-  // $('.full').css({'background': 'url(images/' + randomImage + ') no-repeat center center fixed'});
-
-});
